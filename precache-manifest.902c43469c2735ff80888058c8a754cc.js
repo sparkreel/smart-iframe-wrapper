d12633eb1c92dab6cf415433966682806a532f16
Santiago Calvo <santiago@upshow.tv>
2020-03-13 12:19:48 -0300

    Updates

---
+++ precache-manifest.902c43469c2735ff80888058c8a754cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6eb0ef08409c703ef35f8a8f1288464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95e998beaf17d8d2d72e",
+    "url": "/static/css/main.bcd33509.chunk.css"
+  },
+  {
+    "revision": "9ab5d64bc8c848263732",
+    "url": "/static/js/2.f565803d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f565803d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e998beaf17d8d2d72e",
+    "url": "/static/js/main.a916ff37.chunk.js"
+  },
+  {
+    "revision": "37e2115f42be43f7b87a",
+    "url": "/static/js/runtime-main.2894a674.js"
+  }
+]);
